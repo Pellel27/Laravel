42d6179e6ccb74b0607c4afd5f693f1028e72995
Diallo <hassatoudiallo032@gmail.com>
2022-12-16 09:47:04 +0100

    Création du schéma de bases de données
    Création des tables :
    
    Plat
    etiquette
    categorie
    photo_plat
    photo_ambiance
    reservation
    restau
    actu
    
    Création des relations
    Clé étrangère plat photo_table
    Clé étrangère plat et categorie
    table de jointure etiquette_plat
    correction de l'erreur "Specified key was too long"
    
    corestion du script migrate.sh qui détruit la BDD avant de la reconstruire.

---
+++ database/migrations/2022_12_15_142343_create_reservation_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 100);
+            $table->string('prenom', 100);
+            $table->date('jour');
+            $table->date('heure');
+            $table->integer('nombre_personnes');
+            $table->string('tel', 30);//30 caracteure
+            $table->string('email', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+};
