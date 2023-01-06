64ff898365186f8fbfbad2b0529c492282057479
[Pellel27] <[hassatoudalanda1990@gmail.com]>
2023-01-06 12:02:07 +0100

    Création des variable aléatoire dans seeders

--- database/seeders/PlatSeeder.php
+++ database/seeders/PlatSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Faker;
 
 use App\Models\Categorie;
 use App\Models\PhotoPlat;
@@ -17,9 +18,13 @@ class PlatSeeder extends Seeder
      */
     public function run()
     { 
+        $faker =Faker\Factory::create('fr_FR');
+
         //Toutes les categories
         //::all c'est équivalent d'un SQL 'SELECT* FROM Categorie'
         $categories = Categorie::all();
+        //le nombre d'element dans la collection
+        $categoriesCount = $categories->count();
         //la première categorie
         $categorieEntree = $categories->first();
         //le deuxième catégorie (id 2, plat)
@@ -27,6 +32,9 @@ class PlatSeeder extends Seeder
         $categoriePlat = Categorie::find(2);
         //le troisième catégorie (id 3, plat)
         $categorieDessert = Categorie::find(3);
+        //les autres categories restants
+        $categoriePetitDejeuner = categorie::find(4);
+        $categorieBoisson = categorie::find(5);
 
         //toutes les photos
         $photos = PhotoPlat::all();
@@ -74,9 +82,27 @@ class PlatSeeder extends Seeder
             $plat->photo_plat_id = $platData['photo_plat_id'];
             $plat->categorie_id = $platData['categorie_id'];
             $plat->save();
-          }
         }
-      
 
-    
+        for ($i =0; $i < 100; $i++) {
+            $plat = new Plat();
+            $plat->nom = $faker->words(2, true);
+            $plat->description = $faker->words(10, true);
+            //affectation d'un prix
+            //le prix est aléatoire, compris entre 1 et 50 avec deux chiffres après la virgule
+            $plat->prix = random_int(100, 5000) / 100;
+            //le status épinglé est aléatoire, 0 == false, 1 == true
+            $plat->epingle = (bool) random_int(0, 1);
+
+            //affectation d'une photo
+            $plat->photo_plat_id = $photo->id;
+            //affectation d'une catégorie
+            $categorieIndex = random_int(0, $categoriesCount -1);
+            //on utilise le nombre tiré au hasard pour accéder au Nième élément de la collection de catégories
+            $categorie = $categories->get($categorieIndex);
+            $plat->categorie_id = $categorie->id;
+            //sauvegarde de la BDD
+            $plat->save();
+        }
+    }
 }
--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -13,7 +13,32 @@ class ReservationSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        //
+    { //2 resa avec des données statique
+        //nom Foo
+        //premon Foo
+        //jour 06/01/2023
+        //heure 12:00
+        //tel 0612345678
+        //email foo.foo@example.com
+
+        //nom Bar
+        //premon Bar
+        //jour 06/01/2023
+        //heure 09:00
+        //tel 0612345678
+        //email foo.foo@example.com
+        //nom
+         $faker->lastName();
+         $faker->firstName();
+        // jour
+        $faker->date('y-m-d');
+        //heure
+        $faker->date('h:i');
+        //tel
+        $faker->phoneNumber();
+
+        //email
+        $faker->phoneNumber();
+
     }
 }
